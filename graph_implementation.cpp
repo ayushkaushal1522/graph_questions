5a1b2dbb34de4316a349136eab61d46fdd896a4a
ayushkaushal1522 <ayushkaushal1522@gmail.com>
2023-07-22 16:45:11 +0530

    first commit

--- graph_implementation.cpp
+++ graph_implementation.cpp
@@ -5,23 +5,23 @@ int main() {
 
 	//matrix storation  of the graph
 
-	// int n,m;
-	// cin>>n>>m;
-	// vector<vector<int>> adj(n+1,vector<int>(n+1,0));
-	// for(int i=0;i<m;i++){
-	// 	int u,v;
-	// 	cin>>u>>v;
-	// 	adj[u][v]=1;
-	// 	adj[v][u]=1;
-	// }
+	int n,m;
+	cin>>n>>m;
+	vector<vector<int>> adj(n+1,vector<int>(n+1,0));
+	for(int i=0;i<m;i++){
+		int u,v;
+		cin>>u>>v;
+		adj[u][v]=1;
+		adj[v][u]=1;
+	}
 
 	
-	// for(int i=0;i<n;i++){
-	// 	for(int j=0;j<n;j++){
-	// 		cout<<adj[i][j]<<" ";
-	// 	}
-	// 	cout<<endl;
-	// }
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<adj[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 
 	//adjacency list storation of the graph
 	int n,m;
